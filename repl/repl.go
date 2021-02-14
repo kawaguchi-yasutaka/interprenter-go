453ee7e84ed80cb56d9069d489bc9294e392a5dc
kawaguchi <kawaguchi.yasutaka@gmail.com>
2021-02-14 21:16:48 +0900

    feat: replでevaluateできるように

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"interpreter-go/evaluator"
 	"interpreter-go/lexer"
 	"interpreter-go/parser"
 	"io"
@@ -29,8 +30,12 @@ func Start(in io.Reader, out io.Writer) {
 			printParseErrors(out, parser.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+
+		obj := evaluator.Eval(program)
+		if obj != nil {
+			io.WriteString(out, obj.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
