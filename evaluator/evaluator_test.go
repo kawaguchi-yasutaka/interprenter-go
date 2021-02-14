3cfe2341fe169b5f5178ccb1a04d609f7b6336eb
kawaguchi <kawaguchi.yasutaka@gmail.com>
2021-02-14 21:25:46 +0900

    feat: 真偽値を評価できるように

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -28,6 +28,27 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvaluateBooleanExpresion(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{
+			input:    "true",
+			expected: true,
+		},
+		{
+			input:    "false",
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		obj := testEval(tt.input)
+		testBooleanObject(t, obj, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -44,6 +65,20 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object wrong value got=%d,expected=%d", result.Value, expected)
+		return false
+	}
+	return true
+
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not boolean got=%T(%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object wrong value got=%t,expected=%t", result.Value, expected)
 		return false
 	}
 	return true
